e691aa2be1c37ffad0b3e60ae3e96468ae78f2b5
Ondrej Skutka <oskutka@redhat.com>
2015-05-19 13:24:30 +0200

    Changed Paladeo to ParsingRestaurantGetter - that fixes the problem they have on the page

--- src/main/java/com/redhat/oskutka/rhlp/servlet/Paladeo.java
+++ src/main/java/com/redhat/oskutka/rhlp/servlet/Paladeo.java
@@ -3,14 +3,29 @@ package com.redhat.oskutka.rhlp.servlet;
 import javax.servlet.annotation.WebServlet;
 
 /**
- * Servlet implementation class Kanas
+ * Servlet implementation class Paladeo
  */
 @WebServlet("/Paladeo")
-public class Paladeo extends RestaurantGetter {
-    private static final long serialVersionUID = 3080185090102093278L;
+public class Paladeo extends ParsingRestaurantGetter {
+	private static final long serialVersionUID = 2441840709878659316L;
 
-    protected String getUrl() {
+	protected String getUrl() {
         return "http://www.paladeo.cz/menu";
     }
+	
+	@Override
+	protected String getDayOpeningTag() {
+		return "<ul>";
+	}
+
+	@Override
+	protected String getDayClosingTag() {
+		return "</ul>";
+	}
+	
+	@Override
+	protected int getEndIndex(String result) {
+		return result.indexOf(getDayClosingTag(), getTodayIndex(result)) + getDayClosingTag().length();
+	}
 
 }
