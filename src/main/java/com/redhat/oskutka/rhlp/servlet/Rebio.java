ab32adaf6694e8290c64beed6730c796dbb682a8
Ondrej Skutka <oskutka@redhat.com>
2015-06-30 15:01:07 +0200

    Parsing Rebio PDF

--- src/main/java/com/redhat/oskutka/rhlp/servlet/Rebio.java
+++ src/main/java/com/redhat/oskutka/rhlp/servlet/Rebio.java
@@ -1,16 +1,62 @@
 package com.redhat.oskutka.rhlp.servlet;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.servlet.annotation.WebServlet;
+
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 /**
  * Servlet implementation class Rebio
  */
 @WebServlet("/Rebio")
-public class Rebio extends RestaurantGetter {
+public class Rebio extends ParsingRestaurantGetter {
 	private static final long serialVersionUID = 3901775532572221827L;
 
 	protected String getUrl() {
-        return "http://www.rebio.cz/Rebio-Park/Nase-nabidka/gn-ha.folder.aspx";
-    }
+		return "http://www.rebio.cz/Rebio-Park/Nase-nabidka/Jidelni-listek-Rebio-Park/2897.file.ashx";
+	}
 
+	protected String[] getDays() {
+		return new String[]{"Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Přijímáme objednávky", "Neděle"};
+	}
+
+	protected String getDayOpeningTag() {
+		return "";
+	}
+
+	protected String getDayClosingTag() {
+		return "";
+	}
+	protected String getFreshMenuHTML() {
+		try
+		{
+			StringBuffer result = new StringBuffer();
+			URL url = new URL(getUrl());
+			PDFParser parser = new PDFParser((InputStream) url.getContent());
+			parser.parse();
+			PDFTextStripper stripper = new PDFTextStripper();
+			stripper.setSortByPosition(true);
+			String parsedText = stripper.getText(new PDDocument(parser.getDocument()));
+			for (String line: parseHTML(parsedText).split("\n")) {
+				if (!line.matches("^ *(Saláty, dezerty|Obsahuje Basic menu|Informace o alergenech).*")) {
+					result.append(line.trim() + "\n");
+				}
+			}
+			return "<pre>" + result.toString() + "</pre>";
+		}
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
 }
+
