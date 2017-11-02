66ff0e0e7d2aea3608ff334f16a2a5ff0e31fc5c
Peter Stanko <peter.stanko0@gmail.com>
2017-11-02 12:06:01 +0100

    Added zelena kocka

---
+++ src/main/java/com/redhat/oskutka/rhlp/servlet/ZelenaKocka.java
@@ -0,0 +1,16 @@
+package com.redhat.oskutka.rhlp.servlet;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class Prometheus
+ */
+@WebServlet("/ZelenaKocka")
+public class ZelenaKocka extends RestaurantGetter {
+	private static final long serialVersionUID = 7809185614424754161L;
+
+	protected String getUrl() {
+        return "http://www.zelenakocka.cz/";
+    }
+
+}
