74443b9d1c3848e6f28e1cba91f29c5d30e1d7ba
Ondrej Skutka <oskutka@redhat.com>
2018-02-20 15:43:22 +0100

    Fixing issue #34 again

--- src/main/java/com/redhat/oskutka/rhlp/servlet/CookPoint.java
+++ src/main/java/com/redhat/oskutka/rhlp/servlet/CookPoint.java
@@ -1,6 +1,8 @@
 package com.redhat.oskutka.rhlp.servlet;
 
 import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.text.ParseException;
 
 import javax.servlet.annotation.WebServlet;
@@ -22,7 +24,9 @@ public class CookPoint extends RestaurantGetter {
 
 	@Override
 	protected String getFreshMenuHTML() throws IOException, ParseException {
-		// the site's default language is English, so we first need to switch the page to Czech (it will set Cookies) and only then get the menu 
+		// the site's default language is English, so we first need to switch the page to Czech and only then get the menu
+		CookieManager cm = new CookieManager();
+		CookieHandler.setDefault(cm);
 		getConnection(getPreUrl()).getContent();
 		return super.getFreshMenuHTML();
 	}
